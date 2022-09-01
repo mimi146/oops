050db291952754023323a5bb9658fe2536ffffb7
Mackrin <niroulamilan37@gmail.com>
2022-09-02 00:56:10 +0530

    first added

---
+++ src/main/java/org/example/student.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class student {
+    private  String name;
+    private  int age;
+
+    public  void setName(String name){
+        this.name = name;
+    }
+    public  void setAge(int age){
+        this.age = age;
+    }
+    public String eligibleTovote(){
+        if(this.age >18){
+            return "Yes eligible to vote!!";
+        }
+            return  "Not Eligible to vote";
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
